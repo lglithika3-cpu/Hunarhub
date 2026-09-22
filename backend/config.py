d0e6ba9c7c4eb5b4c98ed610ef3d8997367f98a7
LITHIKA L G <lglithika3@gmail.com>
2026-09-22 22:03:38 +0530

    Fix MySQL driver for Render

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+database_url = os.getenv("DATABASE_URL")
+
+if database_url and database_url.startswith("mysql://"):
+    database_url = database_url.replace("mysql://", "mysql+pymysql://", 1)
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False

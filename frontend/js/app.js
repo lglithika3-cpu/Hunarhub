9d4fc3323dc53adcb4f896bbc41ba0dcc75b78bb
LITHIKA L G <lglithika3@gmail.com>
2026-09-22 21:51:30 +0530

    Prepare frontend API for production

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "";
 
 async function apiRequest(endpoint, options = {}) {
     const response = await fetch(API_BASE_URL + endpoint, {
